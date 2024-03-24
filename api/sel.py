891dec0708c94afb6a5cd5ca9cceb6fb9ccaca11
Miguel Rueda <migruedav@hotmail.com>
2024-03-24 12:13:51 -0600

    Agregado de importación de módulos y código para seleccionar un país aleatorio.

--- api/sel.py
+++ api/sel.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import random
+import time
 
 
 def sel():
@@ -25,9 +27,82 @@ def sel():
             options=options,
         )
 
-        url = "https://www.youmainlyyou.com/"
+        paises = []
 
-        driver.get(url)
-        return driver.title
+        for i in range(70):
+            paises.append("mx")
+        for i in range(15):
+            paises.append("us")
+        for i in range(5):
+            paises.append("es")
+
+        paises.extend(["ar", "co", "pe", "cl", "br", "ec", "gt", "cu", "do", "bo"])
+        pais = random.choice(paises)
+        print(pais)
+
+        proxy_options = {
+            "proxy": {
+                "http": f"http://customer-migruedav-cc-{pais}-sessid-0404864332-sesstime-10:Migruedav1234@pr.oxylabs.io:7777",
+                "https": f"https://customer-migruedav-cc-{pais}-sessid-0404864332-sesstime-10:Migruedav1234@pr.oxylabs.io:7777",
+                "no_proxy": "localhost,127.0.0.1",
+            },
+            "verify_ssl": False,
+            "suppress_connection_errors": False,
+        }
+
+        # driver = webdriver.Chrome(options=options, seleniumwire_options=proxy_options)
+        # driver = webdriver.Chrome(options=options)
+        # driver = webdriver.Chrome(service=service, options=options)
+        driver.get("https://www.youmainlyyou.com/")
+        time.sleep(5)
+        # arts = driver.find_elements(by="css selector", value="a:has(img):has(h3)")
+        # cats = [
+        #     "Wellbeing & Health",
+        #     "Allure & Grooming",
+        #     "Taste",
+        #     "Style",
+        #     "Bon Voyage",
+        #     "Awareness",
+        #     "Gadgets",
+        #     "Furnishing",
+        # ]
+        # cat = driver.find_element(by="link text", value=random.choice(cats))
+        # print("cat", cat.text)
+        # cat.click()
+        # time.sleep(5)
+        driver.quit()
+
+        # # random_time = random.randint(1, 10)
+        # random_time = 3
+        # print("time1", random_time)
+        # time.sleep(random_time)
+        # arts = driver.find_elements(by="css selector", value="a:has(img):has(h3)")
+        # cats = [
+        #     "Wellbeing & Health",
+        #     "Allure & Grooming",
+        #     "Taste",
+        #     "Style",
+        #     "Bon Voyage",
+        #     "Awareness",
+        #     "Gadgets",
+        #     "Furnishing",
+        # ]
+        # cat = driver.find_element(by="link text", value=random.choice(cats))
+        # print("cat", cat.text)
+        # cat.click()
+        # # random_time = random.randint(1, 10)
+        # random_time = 3
+        # print("time2", random_time)
+        # time.sleep(random_time)
+        # arts = driver.find_elements(by="css selector", value="a:has(img):has(h3)")
+        # art = random.choice(arts)
+        # art.click()
+        # # random_time = random.randint(1, 10)
+        # random_time = 3
+        # print("time3", random_time)
+        # time.sleep(random_time)
+        # driver.quit()
+
+        return {"pais": pais, "message": "METDC"}
     except Exception as e:
         return str(e)
