b2105369338b061f535ce76520e49c49820d5346
Miguel Rueda <migruedav@hotmail.com>
2024-03-24 11:38:55 -0600

    Agregadas opciones de configuración para el controlador de Chrome en el archivo sel.py

--- api/sel.py
+++ api/sel.py
@@ -8,7 +8,9 @@ def sel():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
+    options.binary_location = "/root/code/ymy-bt"
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
