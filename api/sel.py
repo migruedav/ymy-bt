d4954dc999e6b985c2b8da4c7f22df57ef579255
Miguel Rueda <migruedav@hotmail.com>
2024-03-24 12:11:02 -0600

    Agregadas opciones de configuración para deshabilitar imágenes y CSS en el perfil del navegador.

--- api/sel.py
+++ api/sel.py
@@ -12,6 +12,12 @@ def sel():
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
 
+        prefs = {
+            "profile.managed_default_content_settings.images": 2,
+            "profile.default_content_setting_values.css": 2,
+        }
+        options.add_experimental_option("prefs", prefs)
+
         driver = webdriver.Chrome(
             service=Service(
                 ChromeDriverManager(driver_version="123.0.6312.58").install()
