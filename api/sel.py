d231662507e088e6a78c11252e35ef4bc8310ea8
Miguel Rueda <migruedav@hotmail.com>
2024-03-27 12:59:29 -0600

    Agrega la opción "--headless" y el user agent string en el archivo sel.py

--- api/sel.py
+++ api/sel.py
@@ -9,12 +9,14 @@ import time
 def sel():
     try:
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--remote-debugging-port=9222")
-        options.binary_location = "/root/code/ymy-bt/google-chrome"
+        # options.add_argument("--remote-debugging-port=9222")
+
+        user_agent_string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+        options.add_argument(f"user-agent={user_agent_string}")
 
         prefs = {
             "profile.managed_default_content_settings.images": 2,
