be0a86a2d47262a1f87380e50cb8b51e8fa10be8
Miguel Rueda <migruedav@hotmail.com>
2024-03-27 12:48:53 -0600

    Actualización de opciones de Selenium***

--- api/sel.py
+++ api/sel.py
@@ -9,11 +9,12 @@ import time
 def sel():
     try:
         options = Options()
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--remote-debugging-port=9222")
+        options.binary_location = "/usr/bin/google-chrome"
 
         prefs = {
             "profile.managed_default_content_settings.images": 2,
