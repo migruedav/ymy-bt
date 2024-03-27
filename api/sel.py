b8d8a27a10002bbc31e4d9e513ee3c681e703f98
Miguel Rueda <migruedav@hotmail.com>
2024-03-27 13:04:41 -0600

    Agregadas opciones de seleniumwire para el proxy

--- api/sel.py
+++ api/sel.py
@@ -52,7 +52,7 @@ def sel():
                 ChromeDriverManager(driver_version="123.0.6312.58").install()
             ),
             options=options,
-            # seleniumwire_options=proxy_options,
+            seleniumwire_options=proxy_options,
         )
 
         driver.get("https://www.youmainlyyou.com/")
