2f08872b77020618109409441487a6b182dde6e7
Miguel Rueda <migruedav@hotmail.com>
2024-03-27 12:47:49 -0600

    Agrega argumento "--remote-debugging-port=9222" a las opciones de Selenium

--- api/sel.py
+++ api/sel.py
@@ -9,10 +9,11 @@ import time
 def sel():
     try:
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--remote-debugging-port=9222")
 
         prefs = {
             "profile.managed_default_content_settings.images": 2,
