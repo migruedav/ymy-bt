92bce45bd5d1e975fa0d55cfc828936e39c4189d
Miguel Rueda <migruedav@hotmail.com>
2024-03-27 12:24:15 -0600

    Desactivar el modo headless en la configuración de opciones de Selenium

--- api/sel.py
+++ api/sel.py
@@ -9,7 +9,7 @@ import time
 def sel():
     try:
         options = Options()
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
